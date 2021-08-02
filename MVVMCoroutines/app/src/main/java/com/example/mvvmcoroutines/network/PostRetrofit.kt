00ddc7d8536b38ede30fdbc2f26e20aa13f4f371
Rafael Ramos <rafaelsramos95@gmail.com>
2021-08-02 17:42:42 +0100

    add get posts by page and by range queries

--- MVVMCoroutines/app/src/main/java/com/example/mvvmcoroutines/network/PostRetrofit.kt
+++ MVVMCoroutines/app/src/main/java/com/example/mvvmcoroutines/network/PostRetrofit.kt
@@ -2,10 +2,17 @@ package com.example.mvvmcoroutines.network
 
 import com.example.mvvmcoroutines.network.entities.PostNetworkEntity
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PostRetrofit {
 
     @GET("posts")
     suspend fun getPosts(): List<PostNetworkEntity>
+
+    @GET("posts")
+    suspend fun getPostsByPage(@Query("_page") pageNumber: Int, @Query("_limit") entriesPerPage: Int): List<PostNetworkEntity>
+
+    @GET("posts")
+    suspend fun getPostsByRange(@Query("_start") startId: Int, @Query("_end") endId: Int): List<PostNetworkEntity>
 
 }
--- MVVMCoroutines/app/src/main/java/com/example/mvvmcoroutines/network/repos/MainRepository.kt
+++ MVVMCoroutines/app/src/main/java/com/example/mvvmcoroutines/network/repos/MainRepository.kt
@@ -5,7 +5,9 @@ import com.example.mvvmcoroutines.mappers.cache.PostCacheMapper
 import com.example.mvvmcoroutines.mappers.network.PostNetworkMapper
 import com.example.mvvmcoroutines.models.Post
 import com.example.mvvmcoroutines.network.PostRetrofit
+import com.example.mvvmcoroutines.network.entities.PostNetworkEntity
 import com.example.mvvmcoroutines.utils.DataState
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -19,14 +21,26 @@ class MainRepository
         private val postNetworkMapper: PostNetworkMapper
     ) {
 
-    suspend fun getPost(): Flow<DataState<List<Post>>> = flow {
+    suspend fun getPostsByPage(page: Int, entriesPerPage: Int): Flow<DataState<List<Post>>> {
+        return coroutineScope {
+            fetchAndStorePosts { postRetrofit.getPostsByPage(page, entriesPerPage) }
+        }
+    }
+
+    suspend fun getPostsByRange(start: Int, end: Int): Flow<DataState<List<Post>>> {
+        return coroutineScope {
+            fetchAndStorePosts { postRetrofit.getPostsByRange(start, end) }
+        }
+    }
+
+    private suspend fun fetchAndStorePosts(getPostsFunc: suspend () -> List<PostNetworkEntity>) = flow {
         emit(DataState.Loading)
 
         //TODO("remove below")
         delay(2000)
 
         try {
-            val networkPosts = postRetrofit.getPosts()
+            val networkPosts = getPostsFunc()
             val posts = postNetworkMapper.mapFromPostList(networkPosts)
 
             for (post in posts) {
@@ -37,7 +51,15 @@ class MainRepository
         } catch (e: Exception) {
             emit(DataState.Error(e))
         }
+    }
 
+    suspend fun fetchCachedPosts(): Flow<DataState<List<Post>>> = flow {
+        try {
+            val cachedPosts = postDao.get()
+            emit(DataState.Success(postCacheMapper.mapFromEntityList(cachedPosts)))
+        } catch (e: Exception) {
+            emit(DataState.Error(e))
+        }
     }
 
 }
