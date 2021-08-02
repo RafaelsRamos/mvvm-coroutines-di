9e43d571767d9ea4827b6486609f3c63b654179a
Rafael Ramos <rafaelsramos95@gmail.com>
2021-08-02 17:43:52 +0100

    use new repo queries in view model

--- MVVMCoroutines/app/src/main/java/com/example/mvvmcoroutines/ui/MainViewModel.kt
+++ MVVMCoroutines/app/src/main/java/com/example/mvvmcoroutines/ui/MainViewModel.kt
@@ -5,7 +5,8 @@ import com.example.mvvmcoroutines.models.Post
 import com.example.mvvmcoroutines.network.repos.MainRepository
 import com.example.mvvmcoroutines.utils.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -17,38 +18,60 @@ class MainViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ): ViewModel() {
 
+    companion object {
+        private const val EntriesPerPage = 10
+    }
+
+    private var page: Int = 0
+
+    private var lastFetchedId = 0
+
     private val _dataState: MutableLiveData<DataState<List<Post>>> = MutableLiveData()
 
     val dataState: LiveData<DataState<List<Post>>> get () = _dataState
 
-    fun setStateEvent(mainStateEvent: MainStateEvent) {
-        viewModelScope.launch {
-
-            when (mainStateEvent) {
-
-                is MainStateEvent.GetPostEvents -> {
-                    mainRepository.getPost()
-                        .onEach {
-                            dataState -> _dataState.value = dataState
-                        }
-                        .launchIn(viewModelScope)
-                }
-
-                is MainStateEvent.None -> {
-                    // Do Nothing
-                }
-
+    // Function responsible for triggering updates
+    @ExperimentalCoroutinesApi
+    fun setStateEvent(stateEvent: StateEvent) {
+        when (stateEvent) {
+            is StateEvent.GetPostsPaginatedEvents -> launchCoroutine { mainRepository.getPostsByPage(++page, EntriesPerPage) }
+            is StateEvent.GetCachedPostsEvents -> launchCoroutine { mainRepository.fetchCachedPosts() }
+            is StateEvent.GetPostsSlicedEvents -> launchCoroutine { mainRepository.getPostsByRange(lastFetchedId, lastFetchedId + EntriesPerPage) }
+            is StateEvent.None -> {
+                // Do Nothing
             }
-
         }
     }
 
+    @ExperimentalCoroutinesApi
+    private fun launchCoroutine(func: suspend () -> Flow<DataState<List<Post>>>) {
+        viewModelScope.launch {
+            func().onEach { dataState ->
+                when (dataState) {
+                    is DataState.Success -> lastFetchedId = dataState.data.getLastFetchedId()
+                    else -> {
+                        // Do Nothing
+                    }
+                }
+                _dataState.value = dataState
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    sealed class StateEvent {
+
+        object GetPostsPaginatedEvents: StateEvent()
+
+        object GetPostsSlicedEvents: StateEvent()
+
+        object GetCachedPostsEvents: StateEvent()
+
+        object None: StateEvent()
+
+    }
 }
 
-sealed class MainStateEvent {
+private fun List<Post>.getLastFetchedId() = this[lastIndex].id
 
-    object GetPostEvents: MainStateEvent()
 
-    object None: MainStateEvent()
 
-}
